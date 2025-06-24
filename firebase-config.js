7b7696990f5907fce9f93772af35c42393f94fab
Niveditha Anumandla <155997659+nivwya@users.noreply.github.com>
2025-06-24 21:07:38 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Paste your Firebase config here. This file is loaded by index.html before script.js.
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDyZbZ-pUynuCY6TyeeoK1aSnWX1fbya2k",
+  authDomain: "hiddengem-c2866.firebaseapp.com",
+  projectId: "hiddengem-c2866",
+  storageBucket: "hiddengem-c2866.appspot.com",
+  messagingSenderId: "888390221341",
+  appId: "1:888390221341:web:339f09b78ad5295fc910f4",
+  measurementId: "G-QX533C2RFP"
+};
+ 
